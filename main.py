cda9f9eb8b712da476e9c06381b8208c0448b8cb
Kazan <andyswebking@gmail.com>
2022-08-03 21:31:28 +0800

    Bot generate and Add auto-replying

---
+++ main.py
@@ -0,0 +1,20 @@
+import discord
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'Now login as: {client.user}')
+    
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content == 'Hi':
+        await message.channel.send('Hello!')
+
+# @client.event
+# async def on_member_join(member):
+#     await member.channel.send(f'Welcome to 開源星手村, {member}')
+
+if __name__ == '__main__':
+    client.run('MTAwNDM0MDM5NjMyOTAyOTY5Mg.GEKPT3.473tO9U9iP9BUPOE5FfiMiGUZ745iAo_yCegek')
