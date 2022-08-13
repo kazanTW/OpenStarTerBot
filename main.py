4ff2d9df2ccba1899e757fe181eaca99274138c1
Kazan <andyswebking@gmail.com>
2022-08-14 00:39:19 +0800

    Update main.py and .gitignore

--- main.py
+++ main.py
@@ -9,12 +9,17 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-    if message.content == 'Hi':
-        await message.channel.send('Hello!')
+    if message.content.startswith('Hi'):
+        tmp = message.content.split(' ', 1)
+        if len(tmp) == 1:
+            await message.channel.send(f'{message.author.mention} 請問你要說什麼？')
+        else:
+            await message.channel.send(f'{message.author.mention} 說了 「{tmp[1]}」')
 
 # @client.event
 # async def on_member_join(member):
 #     await member.channel.send(f'Welcome to 開源星手村, {member}')
 
 if __name__ == '__main__':
-    client.run('TOKEN')
+    TOKEN = open('token.txt').readline()
+    client.run(TOKEN)
